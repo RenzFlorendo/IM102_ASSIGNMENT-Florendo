0f5a804653c84336205c4e6f0264f5f977e3c578
unknown <florendorenzgiancarlo@gmail.com>
2025-09-03 09:59:37 +0800

    ass

---
+++ HW1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("\nEnter string: ");
+            string input = Console.ReadLine();
+
+            if (input == "*")
+                break;
+
+            string lowerInput = input.ToLower();
+            string reversed = "";
+
+            
+            for (int i = lowerInput.Length - 1; i >= 0; i--)
+            {
+                reversed += lowerInput[i];
+            }
+
+           
+            string result = (lowerInput == reversed) ? "Yes" : "No";
+
+           
+            Console.WriteLine("\nOriginal String\tReversed String\tPalindrome?");
+            Console.WriteLine($"  {input,-15}{reversed,-15}{result}");
+        }
+
+        Console.WriteLine("\n//End execution");
+    }
+}
